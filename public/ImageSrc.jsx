e13005e3137d3b0bae41cd13f89992bea0329c3f
carmelshavit <carmel.shavit@gmail.com>
2024-08-24 13:27:24 +0300

    fixed deploy

--- public/ImageSrc.jsx
+++ public/ImageSrc.jsx
@@ -1,8 +1,7 @@
-/** @format */
-
 export const ImagesSrc = {
-	dog: './dog .png',
-
-	cat: './cat.png',
-	catBackground: './cat2.jpg',
+  dog: "./dog .png",
+  // sheltersRescues: "./OrganizationOutline.jpg",
+  // otherAnimal: "./otherAnimal.png",
+  cat: "./cat.png",
+  catBackground: "./cat2.jpg",
 };
